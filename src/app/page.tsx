892653285caf01231c539f8b3d964ee801d3b1d4
Howard <hw@yuanhau.com>
2025-12-26 22:19:06 +0800

    Forgot to map it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ import { Button } from "@/components/ui/button";
 export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center absolute inset-0">
-      <Button>Create Certificate</Button>
+      <Link href="/create">
+        <Button>Create Certificate</Button>
+      </Link>
       <div className="justify-center items-center">
         <h1 className="text-2xl font-bold">Master Certificate</h1>
         <div className="flex flex-row gap-2 justify-center text-center">
